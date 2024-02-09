682bd640220d411f42c65ee09620074de020a1e6
Esmail Emami <esmailemami84@gmail.com>
2024-02-09 11:22:15 +0330

    fix message output model gorm column

--- chat-app/internal/app/models/message.go
+++ chat-app/internal/app/models/message.go
@@ -7,16 +7,16 @@ import (
 )
 
 type MessageOutPutDto struct {
-	ID        uuid.UUID `gorm:"id" json:"id"`
-	Content   string    `gorm:"content" json:"content"`
-	CreatedAt time.Time `gorm:"created_at" json:"createdAt"`
+	ID        uuid.UUID `gorm:"column:id" json:"id"`
+	Content   string    `gorm:"ccolumn:ontent" json:"content"`
+	CreatedAt time.Time `gorm:"column:created_at" json:"createdAt"`
 
-	UserID    uuid.UUID `gorm:"user_id" json:"userId"`
-	FirstName *string   `gorm:"first_name" json:"firstName"`
+	UserID    uuid.UUID `gorm:"column:user_id" json:"userId"`
+	FirstName *string   `gorm:"column:first_name" json:"firstName"`
 	LastName  *string   `gorm:"last_name" json:"lastName"`
 
-	ReplyToID        *uuid.UUID `gorm:"reply_to_id" json:"replyToId"`
-	ReplyToContent   *string    `gorm:"reply_content" json:"replyToContent"`
-	ReplyToFirstName *string    `gorm:"reply_first_name" json:"replyToFirstName"`
-	ReplyToLastName  *string    `gorm:"reply_last_name" json:"replyToLastName"`
+	ReplyToID        *uuid.UUID `gorm:"column:reply_to_id" json:"replyToId"`
+	ReplyToContent   *string    `gorm:"column:reply_content" json:"replyToContent"`
+	ReplyToFirstName *string    `gorm:"column:reply_first_name" json:"replyToFirstName"`
+	ReplyToLastName  *string    `gorm:"column:reply_last_name" json:"replyToLastName"`
 }
